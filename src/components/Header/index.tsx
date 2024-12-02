81150671bc41c43c65f64bc73e7601a822d8aa91
Vitor Comin <vitorgabrielcomin@gmail.com>
2024-12-01 22:37:29 -0300

    adjusting contact with formspree, mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DarkModeIcon } from "../../assets/icons/Icons";
 import Flag from "react-flagkit";
 import { sections } from "../../utils/configs";
 
 const Header: React.FC = () => {
   const [activeSection, setActiveSection] = useState("#home");
+  const navListRef = useRef<HTMLDivElement>(null);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
 
   const handleScrollTo = (id: string) => {
     const section = document.getElementById(id);
@@ -35,6 +37,15 @@ const Header: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  function handleMenuVisibility() {
+    if (navListRef.current) {
+      navListRef.current.classList.toggle("active");
+    }
+    if (mobileMenuRef.current) {
+      mobileMenuRef.current.classList.toggle("active");
+    }
+  }
+
   return (
     <header>
       <nav>
@@ -44,22 +55,24 @@ const Header: React.FC = () => {
           </a>
         </div>
         <div style={{ display: "flex", overflowX: "hidden" }}>
-          {sections?.map((section, index) => (
-            <a
-              key={index}
-              href={section.id}
-              style={{
-                color: activeSection === section.id ? "#D46A00" : "black",
-              }}
-              onClick={(e) => {
-                e.preventDefault();
-                handleScrollTo(section.id);
-              }}
-              className="header-nav-links"
-            >
-              {section.label}
-            </a>
-          ))}
+          <div ref={navListRef} className={"nav-list"}>
+            {sections?.map((section, index) => (
+              <a
+                key={index}
+                href={section.id}
+                style={{
+                  color: activeSection === section.id ? "#D46A00" : "black",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleScrollTo(section.id);
+                }}
+                className="header-nav-links"
+              >
+                {section.label}
+              </a>
+            ))}
+          </div>
           <a className="header-nav-icons">
             <Flag country="BR" />
           </a>
@@ -69,6 +82,15 @@ const Header: React.FC = () => {
           <a className="header-nav-icons">
             <DarkModeIcon />
           </a>
+          <div
+            ref={mobileMenuRef}
+            className="mobile-menu"
+            onClick={handleMenuVisibility}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
         </div>
       </nav>
     </header>
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,9 +1,67 @@
 import PageTitle from "../../components/PageTitle";
+import { useForm, ValidationError } from "@formspree/react";
+import { useRef } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Contact: React.FC = () => {
+  const [state, handleSubmit] = useForm("xjkvpprn");
+  const formRef = useRef<HTMLFormElement>(null);
+  const MySwal = withReactContent(Swal);
+
+  if (state.succeeded) {
+    MySwal.fire({
+      title: "Enviado!",
+      text: "Sua mensagem foi enviada ao meu e-mail com sucesso.",
+      icon: "success",
+      confirmButtonText: "Fechar",
+    });
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+  }
+
   return (
-    <section id="#contact" style={{ marginTop: "5%" }}>
+    <section id="#contact" style={{ marginTop: "1%", flexDirection: "column" }}>
       <PageTitle title={"Contato"} />
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          marginTop: "10%",
+          justifyContent: "center",
+        }}
+      >
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          style={{ display: "flex", flexDirection: "column", width: "50%" }}
+        >
+          <label htmlFor="full-name">Nome Completo</label>
+          <input type="text" name="full-name" id="full-name" />
+          <label style={{ marginTop: "5%" }} htmlFor="email">
+            Endereço de E-mail
+          </label>
+          <input id="email" type="email" name="email" />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <label style={{ marginTop: "5%" }} htmlFor="email">
+            Mensagem
+          </label>
+          <textarea id="message" name="message" />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button
+            className="submit-button"
+            type="submit"
+            disabled={state.submitting}
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
