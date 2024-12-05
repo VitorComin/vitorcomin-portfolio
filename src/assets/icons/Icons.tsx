57293af75648134c6be3a64baca115d5c065c505
Vitor Comin <vitorgabrielcomin@gmail.com>
2024-12-04 23:48:55 -0300

    Adjusnting project carrossel, adjusting reponsivity

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -111,3 +111,27 @@ export const ArrowUpward = () => (
     <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
   </svg>
 );
+
+export const ArrowBack = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#5f6368"
+  >
+    <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+  </svg>
+);
+
+export const ArrowFoward = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#5f6368"
+  >
+    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
+  </svg>
+);
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,16 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useState } from "react";
 import PageTitle from "../../components/PageTitle";
 import { useTranslation } from "react-i18next";
+import { ArrowBack, ArrowFoward } from "../../assets/icons/Icons";
 
 const Projects: React.FC = () => {
   const { t } = useTranslation();
-  const firstRadioRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (firstRadioRef.current) {
-      firstRadioRef.current.checked = true;
-    }
-  }, []);
+  const [currentSlide, setCurrentSlide] = useState(1);
 
   return (
     <section
@@ -18,76 +13,158 @@ const Projects: React.FC = () => {
       style={{ marginTop: "1%", flexDirection: "column" }}
     >
       <PageTitle title={t("projects")} />
-      <div className={"slider"}>
-        <div></div>
-        <div className="slides">
-          <input
-            ref={firstRadioRef}
-            type="radio"
-            name="radio-btn"
-            id="radio1"
-          />
-          <input type="radio" name="radio-btn" id="radio2" />
-          <input type="radio" name="radio-btn" id="radio3" />
-          <input type="radio" name="radio-btn" id="radio4" />
+      <div className="projects-section-body">
+        <button
+          className="projects-arrow-buttons left"
+          onClick={() =>
+            currentSlide === 1
+              ? setCurrentSlide(4)
+              : setCurrentSlide(currentSlide - 1)
+          }
+        >
+          <ArrowBack />
+        </button>
+        <div className={"slider"}>
+          <div className="slides">
+            <input
+              type="radio"
+              name="radio-btn"
+              id="radio1"
+              readOnly
+              checked={currentSlide === 1}
+            />
+            <input
+              type="radio"
+              name="radio-btn"
+              id="radio2"
+              readOnly
+              checked={currentSlide === 2}
+            />
+            <input
+              type="radio"
+              name="radio-btn"
+              id="radio3"
+              readOnly
+              checked={currentSlide === 3}
+            />
+            <input
+              type="radio"
+              name="radio-btn"
+              id="radio4"
+              readOnly
+              checked={currentSlide === 4}
+            />
 
-          <div
-            className={"slide first"}
-            style={{
-              backgroundColor: "red",
-            }}
-          >
-            <div className="project-half-container">
-              <img
-                src="/projeto-portfolio.png"
-                style={{ width: "90%", border: "2px solid black" }}
-              />
+            <div className={"slide first"}>
+              <div className="project-half-container">
+                <img src="/projeto-portfolio.png" />
+              </div>
+              <div className="project-half-container project-description">
+                <strong>{t("working_on_projects")}</strong>
+                <br />
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  pulvinar aliquam elit at tincidunt. In sit amet imperdiet
+                  diam. Donec ornare ligula sed mauris dictum, quis tincidunt
+                  diam accumsan. Nulla quis porttitor sapien. Aliquam commodo
+                  erat ante, in ullamcorper sem consequat sit amet. Donec
+                  scelerisque sapien et eros laoreet, a vestibulum eros feugiat.
+                </span>
+              </div>
             </div>
-            <div className="project-half-container">daniboy</div>
-          </div>
-          <div
-            className={"slide"}
-            style={{
-              backgroundColor: "brown",
-            }}
-          >
-            <div className="project-half-container">eta</div>
-            <div className="project-half-container">daniboy</div>
-          </div>
-          <div
-            className={"slide"}
-            style={{
-              backgroundColor: "yellow",
-            }}
-          >
-            <div className="project-half-container">eta</div>
-            <div className="project-half-container">daniboy</div>
-          </div>
-          <div
-            className={"slide"}
-            style={{
-              backgroundColor: "blue",
-            }}
-          >
-            <div className="project-half-container">eta</div>
-            <div className="project-half-container">daniboy</div>
+            <div className={"slide"}>
+              <div className="project-half-container">
+                <img src="/projeto-portfolio.png" />
+              </div>
+              <div className="project-half-container project-description">
+                <strong>{t("working_on_projects")}</strong>
+                <br />
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  pulvinar aliquam elit at tincidunt. In sit amet imperdiet
+                  diam. Donec ornare ligula sed mauris dictum, quis tincidunt
+                  diam accumsan. Nulla quis porttitor sapien. Aliquam commodo
+                  erat ante, in ullamcorper sem consequat sit amet. Donec
+                  scelerisque sapien et eros laoreet, a vestibulum eros feugiat.
+                </span>
+              </div>
+            </div>
+            <div className={"slide"}>
+              <div className="project-half-container">
+                <img src="/projeto-portfolio.png" />
+              </div>
+              <div className="project-half-container project-description">
+                <strong>{t("working_on_projects")}</strong>
+                <br />
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  pulvinar aliquam elit at tincidunt. In sit amet imperdiet
+                  diam. Donec ornare ligula sed mauris dictum, quis tincidunt
+                  diam accumsan. Nulla quis porttitor sapien. Aliquam commodo
+                  erat ante, in ullamcorper sem consequat sit amet. Donec
+                  scelerisque sapien et eros laoreet, a vestibulum eros feugiat.
+                </span>
+              </div>
+            </div>
+            <div className={"slide"}>
+              <div className="project-half-container">
+                <img src="/projeto-portfolio.png" />
+              </div>
+              <div className="project-half-container project-description">
+                <strong>{t("working_on_projects")}</strong>
+                <br />
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                  pulvinar aliquam elit at tincidunt. In sit amet imperdiet
+                  diam. Donec ornare ligula sed mauris dictum, quis tincidunt
+                  diam accumsan. Nulla quis porttitor sapien. Aliquam commodo
+                  erat ante, in ullamcorper sem consequat sit amet. Donec
+                  scelerisque sapien et eros laoreet, a vestibulum eros feugiat.
+                </span>
+              </div>
+            </div>
+
+            <div className="navigation-auto">
+              <div className="auto-btn1"></div>
+              <div className="auto-btn2"></div>
+              <div className="auto-btn3"></div>
+              <div className="auto-btn4"></div>
+            </div>
           </div>
 
-          <div className="navigation-auto">
-            <div className="auto-btn1"></div>
-            <div className="auto-btn2"></div>
-            <div className="auto-btn3"></div>
-            <div className="auto-btn4"></div>
+          <div className="manual-navigation">
+            <label
+              htmlFor="radio1"
+              onClick={() => setCurrentSlide(1)}
+              className="manual-btn"
+            ></label>
+            <label
+              htmlFor="radio2"
+              onClick={() => setCurrentSlide(2)}
+              className="manual-btn"
+            ></label>
+            <label
+              htmlFor="radio3"
+              onClick={() => setCurrentSlide(3)}
+              className="manual-btn"
+            ></label>
+            <label
+              htmlFor="radio4"
+              onClick={() => setCurrentSlide(4)}
+              className="manual-btn"
+            ></label>
           </div>
         </div>
-        <div></div>
-
-        <div className="manual-navigation">
-          <label htmlFor="radio1" className="manual-btn"></label>
-          <label htmlFor="radio2" className="manual-btn"></label>
-          <label htmlFor="radio3" className="manual-btn"></label>
-          <label htmlFor="radio4" className="manual-btn"></label>
-        </div>
+        <button
+          className="projects-arrow-buttons right"
+          onClick={() =>
+            currentSlide === 4
+              ? setCurrentSlide(1)
+              : setCurrentSlide(currentSlide + 1)
+          }
+        >
+          <ArrowFoward />
+        </button>
       </div>
     </section>
   );
