537061fd4fc4d6aeea92cf6c966d3723c5e88bb0
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-01-04 17:12:45 -0300

    Adjusting some theme colors and removing unused stuff

--- src/assets/icons/Icons.tsx
+++ src/assets/icons/Icons.tsx
@@ -112,30 +112,6 @@ export const ArrowUpward = () => (
   </svg>
 );
 
-export const ArrowBack = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="#5f6368"
-  >
-    <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
-  </svg>
-);
-
-export const ArrowFoward = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="24px"
-    viewBox="0 -960 960 960"
-    width="24px"
-    fill="#5f6368"
-  >
-    <path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
-  </svg>
-);
-
 export const PreviewIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
