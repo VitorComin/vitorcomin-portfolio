7e6b79d85374f36644b4923ab50b43bfeb758e17
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-01-29 11:54:08 -0300

    PageContentTitles test

--- src/__tests__/PageContentTitles.test.tsx
+++ src/__tests__/PageContentTitles.test.tsx
@@ -1,5 +1,15 @@
-//TO DO
+import { render, screen } from "@testing-library/react";
+import PageContentTitles from "../components/PageContentTitles";
 
 describe("<PageContentTitles />", () => {
-  test("should render content title", () => {});
+  test("it should render correctly the title", () => {
+    render(<PageContentTitles title={"Example"} />);
+
+    const titleElement = screen.getByText(/Example/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test("should render the blank title, and not giving an error", () => {
+    expect(() => render(<PageContentTitles title={""} />)).not.toThrow();
+  });
 });
